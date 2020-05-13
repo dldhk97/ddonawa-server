e916d964b3d2c192a7c634b214fa8a81004bda66
dldhk97 <dldhk97@gmail.com>
2020-05-13 17:55:16 +0900

    CSV 파싱 시 콤마별로 split하게 수정

--- src/utility/CSVReader.java
+++ src/utility/CSVReader.java
@@ -11,6 +11,8 @@ import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 
 public class CSVReader {
+	
+	// 하나의 CSV 파일을 읽는 메소드
 	public void read(String filePath) {
 		FileInputStream input;
 	    try {
@@ -19,16 +21,30 @@ public class CSVReader {
 	        decoder.onMalformedInput(CodingErrorAction.IGNORE);
 	        InputStreamReader reader = new InputStreamReader(input, decoder);
 	        BufferedReader bufferedReader = new BufferedReader( reader );
-	        String line = bufferedReader.readLine();					//헤더부분(첫줄)은 읽고 버린다
+	        //String line = bufferedReader.readLine();					// 헤더부분(첫줄)은 읽고 버린다
+	        String line ;
 	        while( (line = bufferedReader.readLine()) != null ) {
-	            System.out.println(line);
+	            String[] splited = line.split(",");
+	            String collect_day = splited[0];
+	            String good_id = splited[1];			// good_id를 저장하면, CSV파싱할 때 같은 상품인지 알 수 있다. 그래서 저장하는게 좋을것 같다..?
+	            String pum_id = splited[2];				// 품목ID는 카테고리별로 분류하기위해 필요하다
+	            String good_name = splited[4];			
+	            String discount_price = splited[6];		//실판매가
+	            System.out.println("수집일자:" + collect_day + ", 상품ID:" + good_id + ", 품목ID:" + pum_id + ", 상품명:" + good_name + ", 가격:" + discount_price);
+	            
+	            // (디버깅용) 가격이 비어있는 항목 찾기
+	            if(discount_price == null || discount_price.isEmpty() || discount_price == "") {
+	            	bufferedReader.close();
+	            	System.out.println("[가격이 비어있는 항목 발견!] " + splited.toString());
+	            	return;
+	            }
 	        }
 	        bufferedReader.close();
-
+	        System.out.println("CSV 파싱 완료!");
 	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();
+	        IOHandler.getInstance().log(e.getMessage());
 	    } catch( IOException e ) {
-	        e.printStackTrace();
+	    	IOHandler.getInstance().log(e.getMessage());
 	    }
 	}
 }
