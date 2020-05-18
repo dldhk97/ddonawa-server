2e74ed038acb82b6016c76856431b44a97ffaeef
dldhk97 <dldhk97@gmail.com>
2020-05-18 16:19:51 +0900

    파이어폭스 겍코 드라이버는 안쓰니까 그냥 지움.

--- src/parser/SeleniumManager.java
+++ src/parser/SeleniumManager.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -22,33 +21,22 @@ public class SeleniumManager {
 		return _instance;
 	}
 	
-	// 크롬으로 크롤링할거면 true로 바꾸고 사용
-	private static final boolean CHROME_MODE = true;
 	private static final int TIMEOUT_CRWAL = 3;
 	
-	// 기본은 파폭으로 동작
-	public static String WEB_DRIVER_ID = "webdriver.gecko.driver";
-	public static String WEB_DRIVER_PATH = ".\\driver\\geckodriver-v0.26.0-win64.exe";
+	// 크롬으로 동작
+	public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
+	public static String WEB_DRIVER_PATH = ".\\\\driver\\\\chromedriver_81.0.4044.138_win32.exe";
 	
 	private WebDriver driver;
 	
 	// 셀레니움 준비	
 	public SeleniumManager() {
-		if(CHROME_MODE) {
-			WEB_DRIVER_ID = "webdriver.chrome.driver";
-			WEB_DRIVER_PATH = ".\\driver\\chromedriver_81.0.4044.138_win32.exe";
-		}
         System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
         
         //드라이버 설정
-        if(CHROME_MODE) {
-        	ChromeOptions options = new ChromeOptions();
-			options.addArguments("headless");					// 크롬이 화면상 뜨지 않게 함
-        	driver = new ChromeDriver(options);
-        }
-        else {
-        	driver = new FirefoxDriver();
-        }
+        ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");					// 크롬이 화면상 뜨지 않게 함
+    	driver = new ChromeDriver(options);
 	}
 	
 	// 암시적 대기 후 크롤 (페이지가 로딩되길 기다렸다가 크롤)
